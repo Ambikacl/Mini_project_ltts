2055079a05dbe1e6f50f5fc3a7e2709189217ea8
Ambika C L <80435833+Ambikacl@users.noreply.github.com>
2021-04-15 19:37:49 +0530

    Add files via upload

---
+++ 3_Implementation/src/percentage.c
@@ -0,0 +1,8 @@
+float percentage(int ch,int num1)
+{
+    float percent;
+    percent = (float)num1 / ch * 100.0;
+    printf("Percentage is %0.3f\n\n",percent);
+    printf("choose next operation:\n");
+    return percent;
+}
