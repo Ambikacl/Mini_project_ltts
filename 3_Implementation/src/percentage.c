fccbbdc08a87885d91e6c1ae4c0b840e5b0ecbc3
Ambika C L <80435833+Ambikacl@users.noreply.github.com>
2021-04-15 19:35:48 +0530

    Delete 3_Implementation/src directory

--- 3_Implementation/src/percentage.c
+++
@@ -1,7 +0,0 @@
-void percentage(int ch,int num1)
-{
-    float percent;
-    percent = (float)num1 / ch * 100.0;
-    printf("Percentage is %0.3f\n\n",percent);
-    printf("choose next operation:\n");
-}
